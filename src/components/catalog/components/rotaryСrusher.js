c9e80cd6f170accef9e764bf8f0d235c45b598ab
Ivan Lebedev <iamil50113@gmail.com>
2025-04-25 12:39:46 +0300

    update

--- src/components/catalog/components/rotaryСrusher.js
+++ src/components/catalog/components/rotaryСrusher.js
@@ -3,7 +3,18 @@ import {StrictMode, useEffect, useState} from "react";
 import Portal from "@/components/catalog/components/portal";
 import GalleryPortal from "@/components/catalog/components/gallery-portal";
 
+const initialArray = [
+    'P-180L-380', 'P-180L-220', 'P-250P', 'P-250C', 'P-300L', 'P-300P', 'Р-300C', 'Р-400L', 'Р-400L', 'Р-400P',
+    'Р-400C', 'Р-500L', 'Р-500P', 'Р-500C', 'Р-600L', 'Р-600P', 'Р-600C', 'Р-700L', 'Р-700P', 'Р-700C', 'Р-800L', 'Р-800P', 'Р-800C', 'Р-1000L', 'Р-1000P'
+]
+
+
 export default function RotaryCrusher() {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchOpen, setSearchOpen] = useState(null)
+
+    const [search, setSearch] = useState([initialArray]);
+
     const [modalItem, setModalItem] = useState(1)
 
     const [modal1Open, setModal1Open] = useState(null)
@@ -12,6 +23,13 @@ export default function RotaryCrusher() {
     const [modal2Item, setModal2Item] = useState(0)
 
     const [modal2Open, setModal2Open] = useState(null)
+
+    const handleSearchChange = (event) => {
+        const query = event.target.value.toLowerCase();
+        setSearchQuery(query);
+        const filtered = initialArray.filter(item => item.toLowerCase().includes(query));
+        setSearch(filtered);
+    };
 
     useEffect(() => {
         console.log("isOpen - ", modal1Open)
@@ -49,13 +67,37 @@ export default function RotaryCrusher() {
             <section className='catalog-sec2'>
                 <div className='base_grid'>
                     <div className='adli catalogp_item_left catalog_input-wrapper'>
-                        <svg width="27" height="26" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M9.98577 18.7434C7.19488 18.7434 4.83312 17.836 2.90048 16.0213C0.967853 14.2065 0.00102499 11.99 8.12843e-07 9.37168C-0.00102337 6.75338 0.965804 4.53686 2.90048 2.72211C4.83516 0.907371 7.19693 0 9.98577 0C12.7746 0 15.1369 0.907371 17.0726 2.72211C19.0083 4.53686 19.9746 6.75338 19.9715 9.37168C19.9715 10.429 19.7923 11.4262 19.4339 12.3634C19.0754 13.3006 18.5889 14.1296 17.9744 14.8505L26.5775 22.9246C26.8592 23.1889 27 23.5253 27 23.9338C27 24.3423 26.8592 24.6788 26.5775 24.9431C26.2959 25.2074 25.9374 25.3396 25.5021 25.3396C25.0669 25.3396 24.7084 25.2074 24.4267 24.9431L15.8236 16.869C15.0555 17.4457 14.1721 17.9023 13.1735 18.2387C12.175 18.5752 11.1124 18.7434 9.98577 18.7434ZM9.98577 15.8598C11.9061 15.8598 13.5387 15.2292 14.8834 13.9681C16.2282 12.707 16.9 11.1749 16.899 9.37168C16.898 7.56847 16.2261 6.03681 14.8834 4.77667C13.5407 3.51654 11.9082 2.88552 9.98577 2.88359C8.06339 2.88167 6.43135 3.5127 5.08967 4.77667C3.748 6.04065 3.07562 7.57232 3.07255 9.37168C3.06947 11.171 3.74185 12.7032 5.08967 13.9681C6.4375 15.2331 8.06953 15.8636 9.98577 15.8598Z"
-                                fill="white" fillOpacity="0.75"/>
-                        </svg>
-                        <input className='text4 catalog_input' type="text" placeholder='Название модели' maxLength="55"
-                               onChange={e => console.log(e)}/>
+                        <div className='catalog_input-area'>
+                            <svg width="27" height="26" viewBox="0 0 27 26" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.98577 18.7434C7.19488 18.7434 4.83312 17.836 2.90048 16.0213C0.967853 14.2065 0.00102499 11.99 8.12843e-07 9.37168C-0.00102337 6.75338 0.965804 4.53686 2.90048 2.72211C4.83516 0.907371 7.19693 0 9.98577 0C12.7746 0 15.1369 0.907371 17.0726 2.72211C19.0083 4.53686 19.9746 6.75338 19.9715 9.37168C19.9715 10.429 19.7923 11.4262 19.4339 12.3634C19.0754 13.3006 18.5889 14.1296 17.9744 14.8505L26.5775 22.9246C26.8592 23.1889 27 23.5253 27 23.9338C27 24.3423 26.8592 24.6788 26.5775 24.9431C26.2959 25.2074 25.9374 25.3396 25.5021 25.3396C25.0669 25.3396 24.7084 25.2074 24.4267 24.9431L15.8236 16.869C15.0555 17.4457 14.1721 17.9023 13.1735 18.2387C12.175 18.5752 11.1124 18.7434 9.98577 18.7434ZM9.98577 15.8598C11.9061 15.8598 13.5387 15.2292 14.8834 13.9681C16.2282 12.707 16.9 11.1749 16.899 9.37168C16.898 7.56847 16.2261 6.03681 14.8834 4.77667C13.5407 3.51654 11.9082 2.88552 9.98577 2.88359C8.06339 2.88167 6.43135 3.5127 5.08967 4.77667C3.748 6.04065 3.07562 7.57232 3.07255 9.37168C3.06947 11.171 3.74185 12.7032 5.08967 13.9681C6.4375 15.2331 8.06953 15.8636 9.98577 15.8598Z"
+                                    fill="white" fillOpacity="0.75"/>
+                            </svg>
+                            <input className='text4 catalog_input' type="text" placeholder='Название модели'
+                                   maxLength="55"
+                                   value={searchQuery}
+                                   onChange={handleSearchChange}
+                                   onFocus={() => setSearchOpen(true)}
+                                   onBlur={() => setSearchOpen(false)}
+                            />
+                        </div>
+                        <ul className='catalog-search-items'
+                            style={searchOpen ? {display: 'block'} : {display: 'none'}}>
+                            {search.map((item, index) => (
+                                <>
+                                    <li className='catalog-search-item' key={index}
+                                        onClick={() => {
+                                            setModal1Open(true);
+                                            document.body.style.overflow = "hidden";
+                                        }}
+                                        onMouseEnter={() => {
+                                            console.log('навели на первого');
+                                            setModalItem(1)
+                                        }}>{item}</li>
+                                    <br/></>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 <div className='catalog-items-sec'>
